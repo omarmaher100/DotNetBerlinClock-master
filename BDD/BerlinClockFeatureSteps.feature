699595baeb04d955dcb0d782f64bd26d8ca39548
omar maher <omarmaher100@yahoo.com>
2016-01-30 22:05:28 +0300

    BerlinClockSolution

---
+++ BDD/BerlinClockFeatureSteps.feature
@@ -0,0 +1,83 @@
+﻿
+Feature: The Berlin Clock
+	As a clock enthusiast
+    I want to tell the time using the Berlin Clock
+    So that I can increase the number of ways that I can read the time
+
+
+Scenario: Midnight 00:00
+When the time is "00:00:00"
+Then the clock should look like
+"""
+Y
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO
+"""
+
+
+Scenario: Middle of the afternoon
+When the time is "13:17:01"
+Then the clock should look like
+"""
+O
+RROO
+RRRO
+YYROOOOOOOO
+YYOO
+"""
+
+Scenario: Just before midnight
+When the time is "23:59:59"
+Then the clock should look like
+"""
+O
+RRRR
+RRRO
+YYRYYRYYRYY
+YYYY
+"""
+
+Scenario: Midnight 24:00
+When the time is "24:00:00"
+Then the clock should look like
+"""
+Y
+RRRR
+RRRR
+OOOOOOOOOOO
+OOOO
+"""
+
+Scenario: first quarter 20:15
+When the time is "20:15:00"
+Then the clock should look like
+"""
+Y
+RRRR
+OOOO
+YYROOOOOOOO
+OOOO
+"""
+
+Scenario: thired quarter 19:39
+When the time is "19:39:00"
+Then the clock should look like
+         """
+         Y
+		 RRRO
+		 RRRR
+		 YYRYYRYOOOO
+		 YYYY
+         """
+Scenario: morning 10:52
+When the time is "10:52:00"
+Then the clock should look like
+         """
+         Y
+		 RROO
+		 OOOO
+		 YYRYYRYYRYO
+		 YYOO
+         """
---
+++ Classes/TimeConverter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock
+{
+    public class TimeConverter : ITimeConverter
+    {
+        public string convertTime(string aTime)
+        {
+            string[] time = aTime.Split(':');
+
+            int hours = int.Parse(time[0]);
+            int minutes = int.Parse(time[1]);
+            int seconds = int.Parse(time[2]);
+
+            char firstSeries = BuildFirstSeriesForSecond(seconds);
+            string secondSeries = BuildSecondSeriesForHour(hours);
+            string thirdSeries = BuildThirdSeriesForHour(hours);
+            string fourthSeries = BuildFourthSeriesForMinutes(minutes);
+            string fifthSeries = BuildFifthSeriesForMinutes(minutes);
+            
+            return $"{firstSeries}\r\n{secondSeries}\r\n{thirdSeries}\r\n{fourthSeries}\r\n{fifthSeries}";
+        }
+
+
+        static char BuildFirstSeriesForSecond(int second)
+        {
+            return (second % 2) == 0 ? 'Y' : 'O';
+        }
+
+        static string BuildSecondSeriesForHour(int hour)
+        {
+            //e.g. 22/5=4; 11/5=2
+            return BuildSeriesForHours(hour / 5);
+        }
+
+        static string BuildThirdSeriesForHour(int hour)
+        {
+            //e.g 22-(22/5)*5=6; 13-(13/5)*5=3
+            return BuildSeriesForHours(hour - (hour / 5) * 5);
+        }
+
+        static string BuildFourthSeriesForMinutes(int min)
+        {
+            int spots = min / 5;
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (i < spots)
+                {
+                    if (i == 2 || i == 5 || i == 8)
+                    {
+                        builder.Append('R');
+                    }
+                    else
+                    {
+                        builder.Append('Y');
+                    }
+                }
+                else
+                {
+                    builder.Append('O');
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        static string BuildFifthSeriesForMinutes(int min)
+        {
+            int spots = min - (min / 5) * 5;
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < 4; i++)
+            {
+                builder.Append(i < spots ? 'Y' : 'O');
+            }
+
+            return builder.ToString();
+        }
+
+        static string BuildSeriesForHours(int spots)
+        {
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < 4; i++)
+            {
+                builder.Append(i < spots ? 'R' : 'O');
+            }
+
+            return builder.ToString();
+        }
+    }
+}
